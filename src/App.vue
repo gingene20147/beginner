f9f2a9710337a3ddcdcd47c44cc4856fbd96debb
gingene20147 <gingene20147@crepowermay.com>
2024-08-05 12:56:06 +0800

    element-plus環境建立

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import TestCom from "./components/TestCom.vue";
+
+type DataType = {
+  id: string;
+  name: string;
+  email: string;
+};
+
+const data: DataType[] = [
+  {
+    id: "1",
+    name: "John",
+    email: "John@example.com",
+  },
+];
+</script>
+
+<template>
+  <TestCom />
+  <ul>
+    <li v-for="user in data">
+      name: {{ user.name }} <br />
+      email: {{ user.email }}
+    </li>
+  </ul>
+</template>
+
+<style scoped></style>
